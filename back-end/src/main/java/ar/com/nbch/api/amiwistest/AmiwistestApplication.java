549978629081f50285d10cc4b252d7841eb09082
Jeremias C. Santacruz <jeresanta@gmail.com>
2021-04-09 10:14:47 -0300

    Initcializando el proyecto

---
+++ back-end/src/main/java/ar/com/nbch/api/amiwistest/AmiwistestApplication.java
@@ -0,0 +1,13 @@
+package ar.com.nbch.api.amiwistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiwistestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmiwistestApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/ar/com/nbch/api/amiwistest/AmiwistestApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.nbch.api.amiwistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmiwistestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
